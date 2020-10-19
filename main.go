cb522b8ee42fa29e998282b949ff35ca3b139f6a
jominjun <mj3029327@naver.com>
2020-10-19 22:28:12 +0900

    .

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/contrib/static"
+  "net/http"
+  "github.com/gin-gonic/gin"
+  "github.com/gin-gonic/contrib/static"
 )
 
 func main() {
 	var r *gin.Engine
 	r = gin.Default()
 
-	r.Use(static.Serve("/", static.LocalFile("./client/build", true)))
+  r.Use(static.Serve("/", static.LocalFile("./client/build", true)))
+  
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+  })
+  
 	r.Run(":8080")
 }
